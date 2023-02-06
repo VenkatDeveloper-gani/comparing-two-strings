4f615c7f602c4789d34987a1a919b8332e8cc7f7
Venkateswarlu Gani <105902350+VenkatDeveloper-gani@users.noreply.github.com>
2023-02-06 07:43:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+first_string=input()
+second_string=input()
+first_string_2=first_string[0:3]
+second_string_2=second_string[0:3]
+print(first_string_2)
+print(second_string_2)
+result = (first_string_2 == second_string_2)
+print(result)
